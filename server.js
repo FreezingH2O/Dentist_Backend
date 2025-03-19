7f6fd1bf80a6f8cccce99a24fc93655c4a127d4e
FreezingH2O <airadajitauleeruk@gmail.com>
2025-03-20 01:39:38 +0700

    fix server.js

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express');  
-const dotenv = require('dotenv');  
-const dentists = require('./routes/dentists');
-const auth = require('./routes/auth');
-const appointments = require('./routes/appointments');
-const cookieParser = require('cookie-parser');
-const connectDB = require('./config/db');  
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet'); 
-const {xss} = require('express-xss-sanitizer');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
-const swaggerJsDoc = require('swagger-jsdoc');  
-const swaggerUI = require('swagger-ui-express');  
+const express = require("express");
+const dotenv = require("dotenv");
+const dentists = require("./routes/dentists");
+const auth = require("./routes/auth");
+const appointments = require("./routes/appointments");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
-// load env vars  
-dotenv.config({path: './config/config.env'});  
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
 
-// Connect to database  
-connectDB();  
+// Connect to database
+connectDB();
 
-const app = express();  
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(mongoSanitize());
@@ -29,43 +29,34 @@ app.use(xss());
 app.use(hpp());
 app.use(cors());
 
-// Rate Limiting  
-const limiter = rateLimit({  
-    windowsMs: 10 * 60 * 1000, // 10 mins  
-    max: 100  
-});  
-
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
 app.use(limiter);
 
-// swagger
-const swaggerOptions = {  
-    swaggerDefinition: {  
-        openapi: '3.0.0',  
-        info: {  
-            title: 'Library API',  
-            version: '1.0.0',  
-            description: 'A simple Express VacQ API'  
-        },  
-        servers:[{url:process.env.HOST+':'+process.env.PORT+'/api/v1'}],
-    },  
-    apis: ['./routes/*.js'],  
-};  
+// Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [{ url: process.env.HOST + "/api/v1" }], // Removed PORT
+  },
+  apis: ["./routes/*.js"],
+};
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);  
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-app.use('/api/v1/dentists',dentists);
-app.use('/api/v1/auth',auth);
-app.use('/api/v1/appointments',appointments);
+// API Routes
+app.use("/api/v1/dentists", dentists);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/appointments", appointments);
 
-
-const PORT = process.env.PORT || 5000;  
-const server = app.listen(PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
-// Handle unhandled promise rejections  
-process.on('unhandledRejection', (err, promise) => {  
-    console.log('Error: ', err.message);  
-    // Close server & exit process  
-    server.close(() => process.exit(1));  
-});  
+// Export the Express app for Vercel
+module.exports = app;
